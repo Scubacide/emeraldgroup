ee60356a88dc90d69fde03334969fb02a876ba94
Corey Fults <coreyfults@gmail.com>
2020-09-24 21:44:35 -0400

    Initial Commit
    WPF Paymentt calculator initiat commit

---
+++ DealershipApp/Database.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using System.Net;
+
+namespace DealershipApp
+{
+    class Database
+    {
+        public Dictionary<string, int> data = new Dictionary<string, int>(); //Where we store rainfall data for the session
+        public double interest;
+        public Database()
+        { //Constructor, initializies the 'DataBase' 
+      
+            using (TextFieldParser csvParser = new TextFieldParser(@"CarData.csv"))
+            {
+                csvParser.CommentTokens = new string[] { "#" };
+                csvParser.SetDelimiters(new string[] { "," });
+                csvParser.HasFieldsEnclosedInQuotes = true;
+                bool firstline = true;
+
+                while (!csvParser.EndOfData)
+                {
+                    if (firstline)
+                    {
+                        firstline = !firstline;
+                        string[] fields = csvParser.ReadFields();
+                        interest = double.Parse(fields[3]) / 100;
+                    } else { 
+                        // Read current line fields, pointer moves to the next line.
+                        //Field 4 is the data in the csv file, field 126 is the "Daily Sum" of the rain in 1/100ths of an inch
+                        string[] fields = csvParser.ReadFields();
+                        string CarModel = fields[0];
+                        int Price = Int32.Parse(fields[1]);
+                        data.Add(CarModel, Price );
+                    }
+                }
+            }
+        }
+
+
+    }
+}
---
+++ DealershipApp/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DealershipApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ComboBox Cars;
+        double interest; //3.8%
+        Database CarDB;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Cars = CarSelection;
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            CarDB = new Database();
+            interest = CarDB.interest;
+            foreach (KeyValuePair<string, int> entry in CarDB.data)
+            {
+                Cars.Items.Add(entry.Key);
+            }
+            Cars.SelectedIndex = 0;
+
+            InterestRate.Content = "Interest: " + interest * 100 + "%";
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int price;
+            CarDB.data.TryGetValue(Cars.SelectedItem.ToString(), out price);
+            CarPrice.Content = "Price: $" + price;
+        }
+
+        private void ValidateNumber(object sender, TextCompositionEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var fullText = textBox.Text.Insert(textBox.SelectionStart, e.Text);
+            double val;
+            e.Handled = !double.TryParse(fullText, out val);
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int price;
+            int months = Int32.Parse(Months.Text);
+            CarDB.data.TryGetValue(Cars.SelectedItem.ToString(), out price);
+            double payment = (price * (1 + (interest / 12) * months)) / months;
+            payment = Math.Ceiling(payment * 100) / 100;
+            Output.Content = "Monthly Payment: $"+ payment;
+        }
+    }
+}
